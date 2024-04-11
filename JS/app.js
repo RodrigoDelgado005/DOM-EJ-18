1992a91e963cf22147c88aae447a03c55ee996da
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-11 17:33:59 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+function reemplazarAtributo() {
+    let puntero1 = document.getElementById('enlace1')
+    let puntero2 = document.getElementById('enlace2')
+    puntero2.setAttribute('href', puntero1.getAttribute ('href'))
+}
